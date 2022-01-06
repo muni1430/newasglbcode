f0eec0b69b0d9e844dea772f1f43589679a3d31a
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-07 00:21:04 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -2,10 +2,10 @@ provider "aws" {
   profile = "default"
   region = "us-east-1"
 }
-resource "aws_launch_configuration" "as_conf" {
-  name_prefix   = var.name_prefix 
-  image_id      = var.image_id
-  instance_type = var.instance_type
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "foobar"
+  image_id      = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
 }
 resource "aws_autoscaling_group" "bar" {
   availability_zones = ["us-east-1c"]
