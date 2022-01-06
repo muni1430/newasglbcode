ae3cda7d899b3e9275e7df2e630be17a14409cd5
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-06 19:24:18 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
