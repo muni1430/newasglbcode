04f3f2f5abf8ad332d6fdf3cafdb16b45aa8d3bf
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-06 22:15:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-
+provider "aws"{
+  region = "us-east-1"
+}
+module "a" {
+  source = "git:: https://github.com/muni1430/newasglbcode.git?ref=main"
+}
+  module "b" {
+    source = "git:: https://github.com/muni1430/newasglbcode.git?ref=main1"
+    target_group_arns = module.a.TG_ARN
+ }
