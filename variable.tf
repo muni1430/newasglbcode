538cc89b3740d06a699bf970422993bf29c40841
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-06 18:39:37 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "name_prefix"{
+type = string
+default = "terraform-lc-example-"
+}
+variable "aws_instances"{
+  type = string
+  default = "i-086045db590bf98f8"
+}
+variable "image_id"{
+  type = string
+  default = "ami-04505e74c0741db8d"
+}
