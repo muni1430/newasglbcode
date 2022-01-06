69fc9ed4eb228d34ed94cfe65a3bace4b56f1dc0
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-06 18:29:13 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,1 +1,17 @@
-
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-example-"
+  image_id      = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+}
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1c"]
+  name                 = "terraform-asg-example"
+  launch_configuration = aws_launch_configuration.as_conf.name
+    desired_capacity   = 1
+    min_size             = 1
+    max_size             = 2
+}
