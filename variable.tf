584e6e98a32efcf5f52e6a31f2a256b98589328c
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-07 09:17:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,22 +1,52 @@
-variable "name_lb" {
-type = string
+variable "name_prefix" {
+  type = string
+  default = "terraform-lc-example-"
 }
-variable "load_balancer_type"{
-  type = string 
+variable "image_id" {
+  type = string
+  default = "ami-0fb653ca2d3203ac1"
+}
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+variable "name" {
+  type = string
+  default = "terraform-asg-example"
+}
+variable "availability_zones" {
+  type = list(string)
+  default = ["us-east-2c"]
+}
+variable "name_lb" {
+  type = string
+  default = "test-lb-tf"
+}
+variable "load_balancer_type" {
+  type = string
+  default = "application"
 }
 variable "security_groups" {
-  type = string
+  type = list(string)
+  default = ["sg-0244e3f85210cc582"]
 }
 variable "subnets" {
-  type = string 
+  type = list(string)
+  default =  ["subnet-bff76bd4","subnet-15e22868"]
 }
-variable "name_TG"{
-type = string
-}
-variable "protocol"{
-type = string
-}
-variable "vpc_id"{
+variable "name_TG" {
   type = string
+  default = "tf-example-lb-tg"
 }
-
+variable "protocol" {
+  type = string
+  default = "HTTP"
+}
+variable "vpc_id" {
+  type = string
+  default = "vpc-6014600b"
+}
+variable "target_id" {
+  type = string
+  default = "i-0080b8172b18ad08a"
+}
